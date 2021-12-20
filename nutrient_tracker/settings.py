cf3d4af9c379e932ae34093b7525535a62664dca
pranita <kalepranita96@gmail.com>
2021-12-20 18:22:30 +0530

    few changes

--- nutrient_tracker/settings.py
+++ nutrient_tracker/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import environ
+
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2q44w(bvp8#(2f@=tuny6_*-4i)0ch*%0p+7@yz=%lw8*h2_ir'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['nutrient-tracker.azurewebsites.net']
 
 
 # Application definition
